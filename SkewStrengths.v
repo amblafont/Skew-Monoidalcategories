c729f3531cd5d4dacb6ccf5d61f5909330a6b69c
Ambroise <chaster_killer@hotmail.fr>
2019-09-25 19:07:12 +0200

    adapt strengths to skew

--- SkewStrengths.v
+++ SkewStrengths.v
@@ -1,7 +1,7 @@
 (**
-  Definition of tensorial strengths between actions over monoidal categories.
+  Definition of tensorial strengths between actions over skew monoidal categories.
 
-  Based on the definitions in the paper "Second-Order and Dependently-Sorted Abstract Syntax" by Marcelo Fiore.
+  Based on the definitions of tensorial strengths for monoidal categories
 **)
 
 Require Import UniMath.Foundations.PartD.
@@ -9,18 +9,18 @@ Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.CategoryTheory.Core.Functors.
 Require Import UniMath.CategoryTheory.Core.NaturalTransformations.
 Require Import UniMath.CategoryTheory.PrecategoryBinProduct.
-Require Import UniMath.CategoryTheory.Monoidal.MonoidalCategories.
-Require Import UniMath.CategoryTheory.Monoidal.Actions.
+Require Import SkewMonoidalCategories.
+Require Import SkewActions.
 
 Local Open Scope cat.
 
 Section A.
 
-Context (Mon_V : monoidal_precat).
+Context (Mon_V : skewmonoidal_precat).
 
-Let V := monoidal_precat_precat Mon_V.
-Let I := monoidal_precat_unit Mon_V.
-Let tensor := monoidal_precat_tensor Mon_V.
+Let V := skewmonoidal_precat_precat Mon_V.
+Let I := skewmonoidal_precat_unit Mon_V.
+Let tensor := skewmonoidal_precat_tensor Mon_V.
 Notation "X ⊗ Y" := (tensor (X , Y)).
 
 Section Strengths_Definition.
@@ -65,7 +65,7 @@ Qed.
 Definition strength_nat : UU := nat_iso strength_dom strength_codom.
 
 Definition strength_triangle_eq (ϛ : strength_nat) :=
-  ∏ (a : A), (pr1 ϛ (a, I)) · (#F (pr1 ϱ a)) = pr1 ϱ' (F a).
+  ∏ (a : A), pr1 ϱ' (F a) · (pr1 ϛ (a, I))   = (#F (pr1 ϱ a)).
 
 Definition strength_pentagon_eq (ϛ : strength_nat): UU := ∏ (a : A), ∏ (x y : V),
   (pr1 χ' ((F a, x), y)) · pr1 ϛ (a, x ⊗ y) =
