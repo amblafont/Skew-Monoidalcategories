b02c229f025d4444869477033a93a4132b74a05d
Ambroise <chaster_killer@hotmail.fr>
2019-09-25 18:56:10 +0200

    Some renamings

--- SkewMonoidalFunctors.v
+++ SkewMonoidalFunctors.v
@@ -12,7 +12,7 @@ Require Import UniMath.CategoryTheory.whiskering.
 
 Local Open Scope cat.
 
-Section Monoidal_Functor.
+Section Skew_monoidal_functor.
 
 Context (Mon_C Mon_D : skewmonoidal_precat).
 
@@ -34,61 +34,61 @@ Let α_D := skewmonoidal_precat_associator Mon_D.
 Let λ_D := skewmonoidal_precat_left_unitor Mon_D.
 Let ρ_D := skewmonoidal_precat_right_unitor Mon_D.
 
-Section Monoidal_Functor_Conditions.
+Section Skew_monoidal_functor_Conditions.
 
 Context (F : C ⟶ D).
 
-Definition monoidal_functor_map_dom : precategory_binproduct C C ⟶ D :=
+Definition skew_monoidal_functor_map_dom : precategory_binproduct C C ⟶ D :=
   functor_composite (pair_functor F F) tensor_D.
 
-Lemma monoidal_functor_map_dom_ok: functor_on_objects monoidal_functor_map_dom =
+Lemma skew_monoidal_functor_map_dom_ok: functor_on_objects skew_monoidal_functor_map_dom =
   λ c, F (ob1 c) ⊗_D F (ob2 c).
 Proof.
   apply idpath.
 Qed.
 
-Definition monoidal_functor_map_codom : precategory_binproduct C C ⟶ D :=
+Definition skew_monoidal_functor_map_codom : precategory_binproduct C C ⟶ D :=
   functor_composite tensor_C F.
 
-Lemma monoidal_functor_map_codom_ok: functor_on_objects monoidal_functor_map_codom =
+Lemma skew_monoidal_functor_map_codom_ok: functor_on_objects skew_monoidal_functor_map_codom =
   λ c, F (ob1 c ⊗_C ob2 c).
 Proof.
   apply idpath.
 Qed.
 
-Definition monoidal_functor_map :=
-  monoidal_functor_map_dom ⟹ monoidal_functor_map_codom.
+Definition skew_monoidal_functor_map :=
+  skew_monoidal_functor_map_dom ⟹ skew_monoidal_functor_map_codom.
 
-Definition monoidal_functor_associativity (μ : monoidal_functor_map) :=
+Definition skew_monoidal_functor_associativity (μ : skew_monoidal_functor_map) :=
   ∏ (x y z : C),
   pr1 μ (x, y) #⊗_D id F(z) · pr1 μ (x ⊗_C y, z) · #F (pr1 α_C ((x, y), z))
   =
   pr1 α_D ((F x, F y), F z) · id (F x) #⊗_D pr1 μ (y, z) · pr1 μ (x, y ⊗_C z).
 
-Definition monoidal_functor_unitality (ϵ : I_D --> F I_C) (μ : monoidal_functor_map) :=
+Definition skew_monoidal_functor_unitality (ϵ : I_D --> F I_C) (μ : skew_monoidal_functor_map) :=
   ∏ (x : C),
   (pr1 λ_D (F x) = ϵ #⊗_D (id (F x)) · pr1 μ (I_C, x) · #F (pr1 λ_C x))
   ×
   (#F (pr1 ρ_C x) = pr1 ρ_D (F x) · (id (F x)) #⊗_D ϵ · pr1 μ (x, I_C)  ).
 
-End Monoidal_Functor_Conditions.
+End Skew_monoidal_functor_Conditions.
 
-Definition lax_monoidal_functor : UU :=
-  ∑ F : C ⟶ D, ∑ ϵ : I_D --> F I_C, ∑ μ : monoidal_functor_map F,
-  (monoidal_functor_associativity F μ) × (monoidal_functor_unitality F ϵ μ).
+Definition lax_skew_monoidal_functor : UU :=
+  ∑ F : C ⟶ D, ∑ ϵ : I_D --> F I_C, ∑ μ : skew_monoidal_functor_map F,
+  (skew_monoidal_functor_associativity F μ) × (skew_monoidal_functor_unitality F ϵ μ).
 
-Definition mk_lax_monoidal_functor (F : C ⟶ D) (ϵ : I_D --> F I_C)
-  (μ : monoidal_functor_map F) (Hass: monoidal_functor_associativity F μ)
-  (Hunit: monoidal_functor_unitality F ϵ μ): lax_monoidal_functor :=
+Definition mk_lax_skew_monoidal_functor (F : C ⟶ D) (ϵ : I_D --> F I_C)
+  (μ : skew_monoidal_functor_map F) (Hass: skew_monoidal_functor_associativity F μ)
+  (Hunit: skew_monoidal_functor_unitality F ϵ μ): lax_skew_monoidal_functor :=
   (F,, (ϵ,, (μ,, (Hass,, Hunit)))).
 
-Definition strong_monoidal_functor : UU :=
-  ∑ L : lax_monoidal_functor,
+Definition strong_skew_monoidal_functor : UU :=
+  ∑ L : lax_skew_monoidal_functor,
   (is_iso (pr1 (pr2 L))) (* ϵ is an iso *)
   ×
   (is_nat_iso (pr1 (pr2 (pr2 L)))). (* μ is an iso *)
 
-End Monoidal_Functor.
+End Skew_monoidal_functor.
 
-Definition strong_monoidal_functor_functor {Mon Mon' : skewmonoidal_precat} (U : strong_monoidal_functor Mon Mon') := pr1 (pr1 U).
-Coercion strong_monoidal_functor_functor : strong_monoidal_functor >-> functor.
+Definition strong_skew_monoidal_functor_functor {Mon Mon' : skewmonoidal_precat} (U : strong_skew_monoidal_functor Mon Mon') := pr1 (pr1 U).
+Coercion strong_skew_monoidal_functor_functor : strong_skew_monoidal_functor >-> functor.
